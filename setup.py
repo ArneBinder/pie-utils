f6a52ea4ccc150f82948085b7960bd328a759aa8
ArneBinder <ArneBinder@users.noreply.github.com>
2022-08-03 21:47:20 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+REQUIRED_PKGS = [
+    "pytorch-ie>=0.8.0,<0.9.0",
+]
+
+TESTS_REQUIRE = [
+    "pytest",
+    "pytest-cov",
+]
+
+QUALITY_REQUIRE = [
+    "pre-commit",
+]
+
+
+EXTRAS_REQUIRE = {
+    "dev": TESTS_REQUIRE + QUALITY_REQUIRE,
+    "tests": TESTS_REQUIRE,
+}
+
+setup(
+    name="pie-utils",
+    version="0.0.1",
+    description="A collection of utilities to easy the usage of PyTorch-IE",
+    author="Arne Binder",
+    author_email="arne.b.binder@gmail.com",
+    url="https://github.com/ArneBinder/pie-utils",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=["pytorch-lightning", "hydra-core"],
+    packages=find_packages(),
+    python_requires=">=3.9.0",
+    install_requires=REQUIRED_PKGS,
+    extras_require=EXTRAS_REQUIRE,
+)
