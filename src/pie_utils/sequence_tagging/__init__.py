2bae6c400ed990343d28d078461ad1494d5b72c5
Bhuvanesh Verma <workoholic24x7@gmail.com>
2022-08-31 21:06:28 +0200

    remove convert_annotation imports

--- src/pie_utils/sequence_tagging/__init__.py
+++ src/pie_utils/sequence_tagging/__init__.py
@@ -1,11 +1,5 @@
-from pie_utils.sequence_tagging.convert_annotation import (
-    convert_span_annotations_to_tag_sequence,
-    convert_tag_sequence_to_span_annotations,
-)
 from pie_utils.sequence_tagging.ill_formed import InvalidTagSequence
 
 __all__ = [
-    "convert_span_annotations_to_tag_sequence",
-    "convert_tag_sequence_to_span_annotations",
     "InvalidTagSequence",
 ]
