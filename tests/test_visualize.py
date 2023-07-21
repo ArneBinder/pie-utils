c07d128414b31750ee1b5f0d532ea2b01d162e96
Arne Binder <arne.binder@dfki.de>
2023-07-21 21:00:42 +0200

    cleanup

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,23 +1,11 @@
 from dataclasses import dataclass
-from typing import Optional
 
 import pytest
 from pytorch_ie.annotations import BinaryRelation, LabeledSpan
-from pytorch_ie.core import AnnotationList, Document, annotation_field
+from pytorch_ie.core import AnnotationList, annotation_field
 from pytorch_ie.documents import TextDocument
 
 from pie_utils.document.visualization import print_document_annotation_graph
-
-
-@dataclass
-class MyDocument(TextDocument):
-    text2: Optional[str] = None
-    entities: AnnotationList[LabeledSpan] = annotation_field(target="text")
-    relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
-    partitions: AnnotationList[LabeledSpan] = annotation_field(target="text")
-    entity_belongs_to_partition: AnnotationList[BinaryRelation] = annotation_field(
-        targets=["entities", "partitions"]
-    )
 
 
 @pytest.mark.parametrize(
@@ -25,6 +13,15 @@ class MyDocument(TextDocument):
     [True, False],
 )
 def test_print_document_annotation_graph(swap_edges):
+    @dataclass
+    class MyDocument(TextDocument):
+        entities: AnnotationList[LabeledSpan] = annotation_field(target="text")
+        relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
+        partitions: AnnotationList[LabeledSpan] = annotation_field(target="text")
+        entity_belongs_to_partition: AnnotationList[BinaryRelation] = annotation_field(
+            targets=["entities", "partitions"]
+        )
+
     document = MyDocument(text="Hello World")
 
     print_document_annotation_graph(
