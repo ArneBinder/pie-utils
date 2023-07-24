7dd00ac027c55498d2131b0f97b347458dea9677
Arne Binder <arne.binder@dfki.de>
2023-07-24 15:11:24 +0200

    add test_get_pie_dataset_type

--- tests/test_dataset_dict.py
+++ tests/test_dataset_dict.py
@@ -99,3 +99,10 @@ def iterable_dataset_dict():
 
 def test_iterable_dataset_dict(iterable_dataset_dict):
     assert set(iterable_dataset_dict) == {"train", "test", "validation"}
+
+
+def test_get_pie_dataset_type():
+    hf_ds = datasets.load_dataset("json", data_dir=DATA_PATH, split="train")
+    hf_ds_iterable = datasets.load_dataset("json", data_dir=DATA_PATH, split="train", streaming=True)
+    assert get_pie_dataset_type(hf_ds) == Dataset
+    assert get_pie_dataset_type(hf_ds_iterable) == IterableDataset
