0849b67b08f843b79357e7f33760558cc0324fc5
ArneBinder <ArneBinder@users.noreply.github.com>
2023-07-24 12:27:52 +0200

    set version="0.3.0" (#41)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ EXTRAS_REQUIRE = {
 
 setup(
     name="pie-utils",
-    version="0.2.1",
+    version="0.3.0",
     description="A collection of utilities to easy the usage of PyTorch-IE",
     author="Arne Binder",
     author_email="arne.b.binder@gmail.com",
