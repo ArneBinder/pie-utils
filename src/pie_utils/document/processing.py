02e197ca3a196f9a4e471d34a39ea1bb7ee584e0
Arne Binder <arne.binder@dfki.de>
2023-07-26 18:57:40 +0200

    remove add_candidate_relations

--- src/pie_utils/document/processing.py
+++ src/pie_utils/document/processing.py
@@ -8,8 +8,6 @@ from typing import Any, Iterable, TypeVar
 from pytorch_ie.annotations import BinaryRelation, LabeledSpan
 from pytorch_ie.core import AnnotationList, Document
 
-from pie_utils.span.slice import distance, is_contained_in
-
 logger = logging.getLogger(__name__)
 
 
@@ -93,98 +91,3 @@ def trim_spans(
 
     return document
 
-
-def get_single_target_layer(document: Document, layer: AnnotationList):
-    if len(layer._targets) != 1:
-        raise Exception(
-            f"the layer is expected to have exactly one target layer, but it has "
-            f"the following targets: {layer._targets}"
-        )
-    target_layer_name = layer._targets[0]
-    return document[target_layer_name]
-
-
-def add_candidate_relations(
-    document: D,
-    label: str = "no_relation",
-    relation_layer: str = "relations",
-    use_predictions: bool = False,
-    partition_layer: str | None = None,
-    max_distance: int | None = None,
-    distance_type: str = "inner",
-    sort_by_distance: bool = True,
-    n_max: int | None = None,
-    # this should not be used during prediction, because it will leak gold relation information!
-    n_max_factor: float | None = None,
-    inplace: bool = True,
-) -> D:
-    if not inplace:
-        document = type(document).fromdict(document.asdict())
-    rel_layer = document[relation_layer]
-    if use_predictions:
-        rel_layer = rel_layer.predictions
-    available_relation_mapping = {(rel.head, rel.tail): rel for rel in rel_layer}
-    if partition_layer is not None:
-        available_partitions = document[partition_layer]
-    else:
-        available_partitions = [None]
-    entity_layer = get_single_target_layer(document=document, layer=rel_layer)
-    if use_predictions:
-        entity_layer = entity_layer.predictions
-
-    candidates_with_distance = {}
-    distances_taken = defaultdict(list)
-    num_relations_in_partition = 0
-    available_rels_within_allowed_distance = set()
-    for partition in available_partitions:
-        if partition is not None:
-            available_entities = [
-                entity
-                for entity in entity_layer
-                if is_contained_in((entity.start, entity.end), (partition.start, partition.end))
-            ]
-        else:
-            available_entities = list(entity_layer)
-        for head in available_entities:
-            for tail in available_entities:
-                if head == tail:
-                    continue
-                d = distance(
-                    (head.start, head.end),
-                    (tail.start, tail.end),
-                    distance_type,
-                )
-                if max_distance is not None and d > max_distance:
-                    continue
-                if (head, tail) in available_relation_mapping:
-                    num_relations_in_partition += 1
-                    distances_taken[available_relation_mapping[(head, tail)].label].append(d)
-                    available_rels_within_allowed_distance.add(
-                        available_relation_mapping[(head, tail)]
-                    )
-                    continue
-                candidates_with_distance[(head, tail)] = d
-    if sort_by_distance:
-        candidates_with_distance_list = sorted(
-            candidates_with_distance.items(), key=lambda item: item[1]
-        )
-    else:
-        candidates_with_distance_list = list(candidates_with_distance.items())
-        random.shuffle(candidates_with_distance_list)
-    n_added = 0
-    if n_max is not None:
-        candidates_with_distance_list = candidates_with_distance_list[:n_max]
-    if n_max_factor is not None:
-        n_max_by_factor = int(len(rel_layer) * n_max_factor)
-        candidates_with_distance_list = candidates_with_distance_list[:n_max_by_factor]
-    # num_total_candidates = len(entities) * len(entities) - len(entities)
-    # update_statistics("num_total_relation_candidates", num_total_candidates)
-    # num_available_relations = len(rel_layer)
-    # update_statistics("num_available_relations", num_available_relations)
-    for (head, tail), d in candidates_with_distance_list:
-        new_relation = BinaryRelation(label=label, head=head, tail=tail)
-        rel_layer.append(new_relation)
-        distances_taken[label].append(d)
-        n_added += 1
-
-    return document
