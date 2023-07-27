9f5fe3c05accf936f1226cb8ec8a7f0c12a9d4a2
ArneBinder <ArneBinder@users.noreply.github.com>
2023-07-27 16:19:11 +0200

    minor fixes (#45)
    * move test_visualize.py to document/test_visualization.py
    
    * use TextBasedDocument instead of deprecated TextDocument

--- src/pie_utils/document/types.py
+++ src/pie_utils/document/types.py
@@ -2,22 +2,22 @@ from dataclasses import dataclass
 
 from pytorch_ie.annotations import BinaryRelation, LabeledSpan
 from pytorch_ie.core import AnnotationList, annotation_field
-from pytorch_ie.documents import TextDocument
+from pytorch_ie.documents import TextBasedDocument
 
 
 @dataclass
-class DocumentWithEntitiesAndRelations(TextDocument):
+class DocumentWithEntitiesAndRelations(TextBasedDocument):
     entities: AnnotationList[LabeledSpan] = annotation_field(target="text")
     relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
 
 
 @dataclass
-class DocumentWithPartitions(TextDocument):
+class DocumentWithPartitions(TextBasedDocument):
     partitions: AnnotationList[LabeledSpan] = annotation_field(target="text")
 
 
 @dataclass
-class DocumentWithEntitiesRelationsAndPartitions(TextDocument):
+class DocumentWithEntitiesRelationsAndPartitions(TextBasedDocument):
     entities: AnnotationList[LabeledSpan] = annotation_field(target="text")
     relations: AnnotationList[BinaryRelation] = annotation_field(target="entities")
     partitions: AnnotationList[LabeledSpan] = annotation_field(target="text")
